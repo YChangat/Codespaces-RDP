188cf5a714297190aa4bdb8ac93c292b1f77c835
YChangat <changatyouness@gmail.com>
2025-02-21 21:47:46 +0100

    Update Windows 10.sh

--- Windows 10.sh
+++ Windows 10.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-sudo apt install -y docker.io
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.gpg > /dev/null
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker --version
+
 mkdir -p dockercom
 cd dockercom
 echo "services:
