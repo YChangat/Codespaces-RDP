2eecf79f252e46b5f5950672141da58418d14d50
YChangat <changatyouness@gmail.com>
2025-02-24 21:47:45 +0000

    Create Windows 7.sh

---
+++ Windows 7.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y docker.io docker-compose
+mkdir -p dockercom
+cat <<EOF > dockercom/Windows_7.yaml
+services:
+  windows:
+    image: dockurr/windows
+    container_name: windows
+    environment:
+      VERSION: 'https://crustywindo.ws/collection/Windows%207/Windows-7-Super-Lite-2017-x86.iso'
+      USERNAME: 'Jor3a-Ti9niya'
+      PASSWORD: 'admin@123'
+      RAM_SIZE: '12G'
+      CPU_CORES: '4'
+      DISK_SIZE: '990G'
+      DISK2_SIZE: '101G'
+    devices:
+      - /dev/kvm
+      - /dev/net/tun
+    cap_add:
+      - NET_ADMIN
+    ports:
+      - "8006:8006"
+      - "3389:3389/tcp"
+      - "3389:3389/udp"
+    stop_grace_period: 2m
+EOF
+
+cd dockercom
+sudo docker-compose -f Windows_7.yaml up
