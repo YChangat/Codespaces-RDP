383a342c3c9152c03442fbdd5474a9bec9937ed3
YChangat <changatyouness@gmail.com>
2025-02-25 10:37:08 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,7 +1,6 @@
 @echo off
 echo Installing Google Chrome ...
-cd /d %~dp0
-powershell -Command "Start-Process 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile 'chrome_installer.exe' -Wait"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://dl.google.com/chrome/install/latest/chrome_installer.exe', 'chrome_installer.exe')"
 start /wait chrome_installer.exe /silent /install
-echo Installation completed successfully !
-
+del /f /q chrome_installer.exe
+echo Installation completed successfully!
