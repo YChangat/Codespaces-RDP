c60c3b64dc710d30c11e75091ccfc93114c569bc
YChangat <changatyouness@gmail.com>
2025-02-25 12:34:01 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,7 +4,8 @@ powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://dl.
 start /wait chrome_installer.exe /silent /install
 del /f /q chrome_installer.exe
 echo Installing Playit gg ...
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://objects.githubusercontent.com/github-production-release-asset-2e65be/445695426/6f14ea3a-35cb-46a5-aa51-b64a8c059e43?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20250225%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250225T110751Z&X-Amz-Expires=300&X-Amz-Signature=46ff86c0440c76897bdf1c9eb99c678a780683886175b02b2b62bd160395f842&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3Dplayit-windows-x86_64-signed.msi&response-content-type=application%2Foctet-stream', 'playit-windows-x86_64-signed.msi')"
-start /wait playit-windows-x86_64-signed.msi /silent /install
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/playit-cloud/playit-agent/releases/download/v0.15.26/playit-windows-x86_64-signed.msi', 'playit-windows-x86_64-signed.msi')"
+start /wait msiexec /i playit-windows-x86_64-signed.msi /quiet /norestart
 del /f /q playit-windows-x86_64-signed.msi
 echo Installation completed successfully!
+pause
